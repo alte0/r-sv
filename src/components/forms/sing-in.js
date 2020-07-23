6dc9bfbf97dbd1fcb11b792b6fed1001697d9349
Максим <makc-dmv@yandex.ru>
2020-07-23 17:03:23 +0500

    add show Success

--- src/components/forms/sing-in.js
+++ src/components/forms/sing-in.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Link } from "react-router-dom";
 import './form.scss';
+import { Success } from "../success/success";
 
 class FormSingIn extends Component {
     constructor(props) {
@@ -11,7 +12,8 @@ class FormSingIn extends Component {
             valuePswd: '',
             isValidForm: false,
             isTypeInputText: false,
-            isValidPswd: false
+            isValidPswd: false,
+            showSuccess: false
         }
 
         this.state = this.initialState;
@@ -51,44 +53,55 @@ class FormSingIn extends Component {
         this.setState((state) => ({isTypeInputText: !state.isTypeInputText}));
     }
 
+    handleSubmit = (evt) => {
+        evt.preventDefault();
+        this.setState({showSuccess: true});
+    }
+
     render() {
         return (
-            <form className={"form"}>
-                <div className={"form__field"}>
-                    <input className={"form__input"} type={"text"} name={"login"} id={"login"} required
-                           value={this.state.valueLogin}
-                           onChange={this.handleChangeLogin}/>
-                    <label className={"form__label form__label--absolute"} htmlFor={"login"}>Email или номер телефона</label>
-                </div>
-                <div className={"form__field"}>
-                    <input className={"form__input"}  name={"pswd"} id={"pswd"} required
-                           type={this.state.isTypeInputText ? "text" : "password" }
-                           value={this.state.valuePswd}
-                           onChange={this.handleChangePswd}
-                           pattern={"((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})"}
-                            />
-                    <label className={"form__label form__label--absolute"} htmlFor={"pswd"}>Пароль</label>
-                    <button className={"form__switch-input-type"} type={"button"}
-                        onClick={this.handleClickToggle}>
-                        <svg className={"eye"} width={16} height={12} viewBox="0 0 19 14" fill="none">
-                            <path
-                                d="M12.113 7A2.572 2.572 0 116.97 7a2.572 2.572 0 015.143 0v0z"
-                            />
-                            <path
-                                d="M1.363 7a8.575 8.575 0 0116.358 0A8.575 8.575 0 011.363 7v0z"
-                            />
-                        </svg>
-                        <span className={"visually-hidden"}>Посмотреть или скрыть пароль</span>
-                    </button>
-                </div>
-                <div className={"form__field"}>
-                    <button className={"btn form__btn"} type={"submit"} disabled={!this.state.isValidForm}>Войти </button>
-                </div>
-                <div className={"form__field form__field--text-center"}>
-                    <span className={"form__question"}>Нет аккаунта?</span>
-                    <Link className={"form__link"} to="/sing-up">Зарегистрироваться</Link>
-                </div>
-            </form>
+            <React.Fragment>
+                <form className={"form"}
+                      onSubmit={this.handleSubmit}>
+                    <div className={"form__field"}>
+                        <input className={"form__input"} type={"text"} name={"login"} id={"login"} required
+                               value={this.state.valueLogin}
+                               onChange={this.handleChangeLogin}/>
+                        <label className={"form__label form__label--absolute"} htmlFor={"login"}>Email или номер телефона</label>
+                    </div>
+                    <div className={"form__field"}>
+                        <input className={"form__input"}  name={"pswd"} id={"pswd"} required
+                               type={this.state.isTypeInputText ? "text" : "password" }
+                               value={this.state.valuePswd}
+                               onChange={this.handleChangePswd}
+                               pattern={"((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})"}
+                        />
+                        <label className={"form__label form__label--absolute"} htmlFor={"pswd"}>Пароль</label>
+                        <button className={"form__switch-input-type"} type={"button"}
+                                onClick={this.handleClickToggle}>
+                            <svg className={this.state.isTypeInputText ? "eye eye--on" : "eye" } width={16} height={12} viewBox="0 0 19 14" fill="none">
+                                <path
+                                    d="M12.113 7A2.572 2.572 0 116.97 7a2.572 2.572 0 015.143 0v0z"
+                                />
+                                <path
+                                    d="M1.363 7a8.575 8.575 0 0116.358 0A8.575 8.575 0 011.363 7v0z"
+                                />
+                            </svg>
+                            <span className={"visually-hidden"}>Посмотреть или скрыть пароль</span>
+                        </button>
+                    </div>
+                    <div className={"form__field"}>
+                        <button className={"btn form__btn"} type={"submit"} disabled={!this.state.isValidForm}>Войти </button>
+                    </div>
+                    <div className={"form__field form__field--text-center"}>
+                        <span className={"form__question"}>Нет аккаунта?</span>
+                        <Link className={"form__link"} to="/sing-up">Зарегистрироваться</Link>
+                    </div>
+                </form>
+                {
+                    this.state.showSuccess ? <Success text={"авторизированны"}/> : ""
+                }
+            </React.Fragment>
         )
     }
 }
--- src/components/forms/sing-up.js
+++ src/components/forms/sing-up.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import {Link } from "react-router-dom";
+import {Link, withRouter } from "react-router-dom";
 import InpuMask from 'react-input-mask';
 import './form.scss';
+import {Success} from "../success/success";
 
 class FormSingUp extends Component {
     constructor(props) {
@@ -18,7 +19,8 @@ class FormSingUp extends Component {
             isChecked: false,
             isValidPhone: false,
             isValidPswd: false,
-            isValidEmail: false
+            isValidEmail: false,
+            showSuccess: false
         }
 
         this.state = this.initialState;
@@ -109,78 +111,95 @@ class FormSingUp extends Component {
         }));
     }
 
+    handleSubmit = (evt) => {
+        evt.preventDefault();
+        const { history } = this.props;
+
+        this.setState({showSuccess: true});
+
+        setTimeout(() => {
+            history.push("/")
+        }, 3500)
+    }
+
     render() {
         return (
-            <form className={"form"}>
-                <div className={"form__field"}>
-                    <input className={"form__input"} type={"text"} name={"name"} id={"name"} required
-                           value={this.state.valueName}
-                           onChange={this.handleChangeName}/>
-                    <label className={"form__label form__label--absolute"} htmlFor={"name"}>Имя</label>
-                </div>
-                <div className={"form__field"}>
-                    <input className={"form__input"} type={"text"} name={"login"} id={"login"} required
-                           value={this.state.valueLogin}
-                           onChange={this.handleChangeLogin}/>
-                    <label className={"form__label form__label--absolute"} htmlFor={"login"}>Никнейм</label>
-                </div>
-                <div className={"form__field"}>
-                    <input className={"form__input"} type={"email"} name={"email"} id={"email"} required
-                           value={this.state.valueEmail}
-                           onChange={this.handleChangeEmail}
-                            pattern={".+@.+\\..+"}
-                    />
-                    <label className={"form__label form__label--absolute"} htmlFor={"email"}>Email</label>
-                </div>
-                <div className={"form__field"}>
-                    <InpuMask
-                        className={"form__input"}
-                        mask="+7\ 999 999 99 99"
-                        maskPlaceholder="_"
-                        name={"tel2"}
-                        id={"tel2"}
-                        value={this.state.valueTel}
-                        onChange={this.handleChangeTel}
-                        required/>
-                    <label className={"form__label form__label--absolute"} htmlFor={"tel"}>Телефон</label>
-                </div>
-                <div className={"form__field"}>
-                    <input className={"form__input"} name={"pswd"} id={"pswd"} required
-                           type={this.state.isTypeInputText ? "text" : "password" }
-                           value={this.state.valuePswd}
-                           onChange={this.handleChangePswd}
-                           pattern={"((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})"}
+            <React.Fragment>
+                <form className={"form"}
+                      onSubmit={this.handleSubmit}>
+                    <div className={"form__field"}>
+                        <input className={"form__input"} type={"text"} name={"name"} id={"name"} required
+                               value={this.state.valueName}
+                               onChange={this.handleChangeName}/>
+                        <label className={"form__label form__label--absolute"} htmlFor={"name"}>Имя</label>
+                    </div>
+                    <div className={"form__field"}>
+                        <input className={"form__input"} type={"text"} name={"login"} id={"login"} required
+                               value={this.state.valueLogin}
+                               onChange={this.handleChangeLogin}/>
+                        <label className={"form__label form__label--absolute"} htmlFor={"login"}>Никнейм</label>
+                    </div>
+                    <div className={"form__field"}>
+                        <input className={"form__input"} type={"email"} name={"email"} id={"email"} required
+                               value={this.state.valueEmail}
+                               onChange={this.handleChangeEmail}
+                                pattern={".+@.+\\..+"}
                         />
-                    <label className={"form__label form__label--absolute"} htmlFor={"pswd"}>Пароль</label>
-                    <button className={"form__switch-input-type"} type={"button"}
-                            onClick={this.handleClickToggle}>
-                        <svg className={"eye"} width={16} height={12} viewBox="0 0 19 14" fill="none">
-                            <path
-                                d="M12.113 7A2.572 2.572 0 116.97 7a2.572 2.572 0 015.143 0v0z"
+                        <label className={"form__label form__label--absolute"} htmlFor={"email"}>Email</label>
+                    </div>
+                    <div className={"form__field"}>
+                        <InpuMask
+                            className={"form__input"}
+                            mask="+7\ 999 999 99 99"
+                            maskPlaceholder="_"
+                            name={"tel2"}
+                            id={"tel2"}
+                            value={this.state.valueTel}
+                            onChange={this.handleChangeTel}
+                            required/>
+                        <label className={"form__label form__label--absolute"} htmlFor={"tel"}>Телефон</label>
+                    </div>
+                    <div className={"form__field"}>
+                        <input className={"form__input"} name={"pswd"} id={"pswd"} required
+                               type={this.state.isTypeInputText ? "text" : "password" }
+                               value={this.state.valuePswd}
+                               onChange={this.handleChangePswd}
+                               pattern={"((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})"}
                             />
-                            <path
-                                d="M1.363 7a8.575 8.575 0 0116.358 0A8.575 8.575 0 011.363 7v0z"
-                            />
-                        </svg>
-                        <span className={"visually-hidden"}>Посмотреть или скрыть пароль</span>
-                    </button>
-                </div>
-                <div className={"form__field form__field--agreement"}>
-                    <input className={"form__input form__input--agreement visually-hidden"} type={"checkbox"} name={"agreement"} id={"agreement"} required
-                           checked={this.state.isChecked}
-                           onChange={this.handleChangeCheckbox}/>
-                    <label className={"form__label form__label--agreement"} htmlFor={"agreement"}>Я даю свое согласие на обработку персональных данных</label>
-                </div>
-                <div className={"form__field"}>
-                    <button className={"btn form__btn"} type={"submit"} disabled={!this.state.isValidForm}>Зарегистрироваться </button>
-                </div>
-                <div className={"form__field form__field--text-center"}>
-                    <span className={"form__question"}>Есть аккаунт?</span>
-                    <Link className={"form__link"} to="/">Войти</Link>
-                </div>
-            </form>
+                        <label className={"form__label form__label--absolute"} htmlFor={"pswd"}>Пароль</label>
+                        <button className={"form__switch-input-type"} type={"button"}
+                                onClick={this.handleClickToggle}>
+                            <svg className={this.state.isTypeInputText ? "eye eye--on" : "eye" } width={16} height={12} viewBox="0 0 19 14" fill="none">
+                                <path
+                                    d="M12.113 7A2.572 2.572 0 116.97 7a2.572 2.572 0 015.143 0v0z"
+                                />
+                                <path
+                                    d="M1.363 7a8.575 8.575 0 0116.358 0A8.575 8.575 0 011.363 7v0z"
+                                />
+                            </svg>
+                            <span className={"visually-hidden"}>Посмотреть или скрыть пароль</span>
+                        </button>
+                    </div>
+                    <div className={"form__field form__field--agreement"}>
+                        <input className={"form__input form__input--agreement visually-hidden"} type={"checkbox"} name={"agreement"} id={"agreement"} required
+                               checked={this.state.isChecked}
+                               onChange={this.handleChangeCheckbox}/>
+                        <label className={"form__label form__label--agreement"} htmlFor={"agreement"}>Я даю свое согласие на обработку персональных данных</label>
+                    </div>
+                    <div className={"form__field"}>
+                        <button className={"btn form__btn"} type={"submit"} disabled={!this.state.isValidForm}>Зарегистрироваться </button>
+                    </div>
+                    <div className={"form__field form__field--text-center"}>
+                        <span className={"form__question"}>Есть аккаунт?</span>
+                        <Link className={"form__link"} to="/">Войти</Link>
+                    </div>
+                </form>
+                {
+                    this.state.showSuccess ? <Success text={"зарегистрированы"}/> : ""
+                }
+            </React.Fragment>
             )
     }
 }
 
-export default FormSingUp;
+export default withRouter(FormSingUp);
