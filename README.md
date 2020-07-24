65e8b5895d878bf1ca7f1d43bf96639b8df098df
Максим <makc-dmv@yandex.ru>
2020-07-24 12:00:24 +0500

    Update 2020-07-24T07:00:18.314Z

--- README.md
+++
@@ -1,1 +0,0 @@
-#### [App на github.io](https://alte0.github.io/r-sv/)
---
+++ precache-manifest.3809959378959a7d261bbd14a75610c0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61d5762fdb8d709086090123c1ace69",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81c5ba93437c0a1a370d",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "949be4cb7d8a1ea6e633",
+    "url": "./static/css/main.40eeaf2d.chunk.css"
+  },
+  {
+    "revision": "9a3bf7acae14d9b5ed5a88458106b58b",
+    "url": "./static/fonts/Roboto-Medium.woff"
+  },
+  {
+    "revision": "50d01d3e6c994995bcaf829e63d53d1a",
+    "url": "./static/fonts/Roboto-Medium.woff2"
+  },
+  {
+    "revision": "94dac78eee406a8c8f0406b69b85ac2b",
+    "url": "./static/fonts/Roboto-Regular.woff"
+  },
+  {
+    "revision": "9feb0110b6dff9ee2b9ebd17f7a1aee6",
+    "url": "./static/fonts/Roboto-Regular.woff2"
+  },
+  {
+    "revision": "3793d41eb5484e1b08b9639e9779345d",
+    "url": "./static/fonts/SFCompactDisplay-Medium.woff"
+  },
+  {
+    "revision": "6355a010e29ae65e46c032288bcabd72",
+    "url": "./static/fonts/SFCompactDisplay-Medium.woff2"
+  },
+  {
+    "revision": "98fc64c3ccc561bbb2e299d00c2d0920",
+    "url": "./static/fonts/SFCompactDisplay-Regular.woff"
+  },
+  {
+    "revision": "1a2dd3166014f54fce30c05a4bc66232",
+    "url": "./static/fonts/SFCompactDisplay-Regular.woff2"
+  },
+  {
+    "revision": "edba79eaeedbc54f59b7253e99d80ff7",
+    "url": "./static/fonts/SFCompactDisplay-Semibold.woff"
+  },
+  {
+    "revision": "e14c646c892156b6c82ecab535aa5c25",
+    "url": "./static/fonts/SFCompactDisplay-Semibold.woff2"
+  },
+  {
+    "revision": "81c5ba93437c0a1a370d",
+    "url": "./static/js/2.56f421fc.chunk.js"
+  },
+  {
+    "revision": "567ff855e89811c94bb38895efc27afb",
+    "url": "./static/js/2.56f421fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "949be4cb7d8a1ea6e633",
+    "url": "./static/js/main.b38152fc.chunk.js"
+  },
+  {
+    "revision": "db32bdce09304312fd8e",
+    "url": "./static/js/runtime-main.9649f36b.js"
+  }
+]);
