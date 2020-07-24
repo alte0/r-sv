ee1fae698c3786fec9c09575770e105b7a4d6689
Максим <makc-dmv@yandex.ru>
2020-07-24 12:15:06 +0500

    fix path for github.io

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.scss';
 const App = () => {
 return (
     <div className={"app-container"}>
-      <Router>
+      <Router basename="/r-sv/" >
           <Route
               exact
               path="/">
